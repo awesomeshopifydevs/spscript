dac78fcd42e740e74c6b440b478f699b1e6e23ff
Awesome Devs <awesomeshopifydevs@gmail.com>
2021-04-07 16:01:54 +0530

    added changes for the script

--- base-spscript.js
+++ base-spscript.js
@@ -1,1 +1,20 @@
-console.log('Thank for the business with us');
+console.log("Thank for the business with us");
+var loadJS = function (url, implementationCode, location) {
+  //url is URL of external file, implementationCode is the code
+  //to be called from the file, location is the location to
+  //insert the <script> element
+
+  var scriptTag = document.createElement("script");
+  scriptTag.src = url;
+
+  scriptTag.onload = implementationCode;
+  scriptTag.onreadystatechange = implementationCode;
+
+  location.appendChild(scriptTag);
+};
+
+var celebrationStart = function () {
+  console.log('jquery is loaded');
+};
+
+loadJS("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js", celebrationStart, document.body);
